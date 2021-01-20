5c6cbddf48ffd5ee8f3539df9256dc01031f59c2
Sangavi Ashwanth <sangaviashwanth19k@gmail.com>
2021-01-20 13:21:15 +0530

    added

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, viking1;
-var backgroundImg,platform;
+var backgroundImg,platform1;
 
 function preload() {
     backgroundImg = loadImage("sprites/backGround.png");
@@ -17,7 +17,7 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    platform1 = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,100);
     box2 = new Box(920,320,70,100);
@@ -60,5 +60,5 @@ function draw(){
     log5.display();
 
     rock.display();
-    platform.display();
+    platform1.display();
 }
